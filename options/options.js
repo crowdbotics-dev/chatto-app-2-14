cdc5d77b9d547b757b5b2016c24fe4dfd612a194
Crowdbotics <engineering@crowdbotics.com>
2023-06-29 13:11:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "chatto-app-2-14",
+  url: "https://chatto-app-2-14.botics.co",
+  api: "https://chatto-app-2-14.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
